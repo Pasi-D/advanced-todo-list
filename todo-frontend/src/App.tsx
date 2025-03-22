02640fe31bf630be3aa4a2e6989cf1bc5479872f
Pasindu Dilshan <pasindu_dilshan@outlook.com>
2025-03-22 17:37:41 +0530

    Refactor code to use consistent double quotes and improve formatting

--- todo-frontend/src/App.tsx
+++ todo-frontend/src/App.tsx
@@ -1,21 +1,20 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Landing from './pages/Landing';
-import NotFound from './pages/NotFound';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Landing from "./pages/Landing";
+import NotFound from "./pages/NotFound";
 
-import './App.css';
+import "./App.css";
 
 const App: React.FC = () => {
-
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Landing />} />
+          <Route path="/" element={<Landing />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
