5439aa706d3bbefdb5089dc47949f833b2860215
Pasindu Dilshan <pasindu_dilshan@outlook.com>
2025-03-23 11:56:05 +0530

    chore: update eslint rules

--- todo-frontend/eslint.config.js
+++ todo-frontend/eslint.config.js
@@ -43,6 +43,7 @@ export default tseslint.config(
       }],
       "@typescript-eslint/no-unused-vars": ["warn", { "varsIgnorePattern": "^_", "argsIgnorePattern": "^_" }],
       "@typescript-eslint/no-explicit-any": ["warn", { "ignoreRestArgs": true }],
+      "react/no-children-prop": "off",
     },
   },
 )
